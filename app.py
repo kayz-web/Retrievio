ec808b5bc4106423ed9bf1fabf797a9b081dc2b7
garvmahajan20 <garvmahajan20107@gmail.com>
2026-05-31 13:35:34 +0530

    Connect Flask to SQLite

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import sqlite3
 app=Flask(__name__)
+DATABASE="lost_found.db"
+def get_db_connection():
+    connection=sqlite3.connect(DATABASE)
+    connection.row_factory=sqlite3.Row
+    return connection
 @app.route("/")
 def home():
     return "Lost & Found backend running"
