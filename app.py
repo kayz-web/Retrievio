dff3b2814f5f966ad4889634feb87f2cc3bfe39c
garvmahajan20 <garvmahajan20107@gmail.com>
2026-05-31 14:20:15 +0530

    Save lost and found items to SQLite

--- app.py
+++ app.py
@@ -16,15 +16,47 @@ def report_lost():
         description=request.form["description"]
         location=request.form["location"]
         contact=request.form["contact"]
+        connection = get_db_connection()
+        cursor=connection.cursor()
+        cursor.execute("""
+            INSERT INTO items
+            (type,name,description,location,contact,status,date_posted)
+            VALUES(?,?,?,?,?,?,DATE('now'))"""
+        ,(
+            "Lost",
+            name,
+            description,
+            location,
+            contact,
+            "Active"
+        ))
+        connection.commit()
+        connection.close()
         return f"Lost item recieved : {name}"
     return render_template("report_lost.html")
-@app.route("/report-found",methods=["GET,POST"])
+@app.route("/report-found",methods=["GET","POST"])
 def report_found():
     if request.method=="POST":
         name=request.form["name"]
         description=request.form["description"]
         location=request.form["location"]
         contact=request.form["contact"]
+        connection = get_db_connection()
+        cursor=connection.cursor()
+        cursor.execute("""
+            INSERT INTO items
+            (type,name,description,location,contact,status,date_posted)
+            VALUES(?,?,?,?,?,?,DATE('now'))"""
+        ,(
+            "Found",
+            name,
+            description,
+            location,
+            contact,
+            "Active"
+        ))
+        connection.commit()
+        connection.close()
         return f"Found item recieved : {name}"
     return render_template("report_found.html")
 if __name__=="__main__":
